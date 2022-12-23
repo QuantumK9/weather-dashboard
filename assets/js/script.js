cb5a1195b7e6e9dedc8c7a9bc3e95165c3d176d1
QuantumK <quantumk9@hotmail.com>
2022-12-23 14:36:42 +0000

    refactor AJAX calls and data manipulation into a separate function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,102 +1,98 @@
-// console.log("yo");
-let cities = [
+// Local state of cities searched and their data
+var cities = [
   // {
   //   cityName: "",
   //   data: [],
   // },
 ];
-
+// taking cities from localStorage if any and rendering buttons
 $(document).ready(function () {
   var storedCities = JSON.parse(localStorage.getItem("cities"));
   if (storedCities !== null) {
     cities = storedCities;
   }
+  renderButtons(cities);
 });
+
+// Adding click event listeners to all elements with a class of "movie"
+// $(document).on("click", ".city", displayCityInfo);
 
 // event listenter on search button
 $("#search-button").on("click", function (event) {
-  // var person = $(this).attr("data-city");
   event.preventDefault();
-  // console.log("event triggered");
-  // This line of code will grab the input from the textbox
   var city = $("#search-input").val().trim();
-  // console.log(city);
 
+  getCityForecastAndDisplay(city);
   // AJAX request from Geolocation API for lon and lat of a city
-  var API_KEY = "678a5dc678523498214a6c93764d73f1";
-  var geolocationQueryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
+  // var API_KEY = "678a5dc678523498214a6c93764d73f1";
+  // var geolocationQueryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
 
-  $.ajax({
-    url: geolocationQueryURL,
-    method: "GET",
-  }).then(function (response) {
-    // console.log(response[0].lat, response[0].lon);
+  // $.ajax({
+  //   url: geolocationQueryURL,
+  //   method: "GET",
+  // }).then(function (response) {
+  //   var lat = response[0].lat;
+  //   var lon = response[0].lon;
 
-    var lat = response[0].lat;
-    var lon = response[0].lon;
+  //   var queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=50&appid=${API_KEY}`;
 
-    var queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=50&appid=${API_KEY}`;
-    // console.log(`queryURL: ${queryURL} lat: ${lat} lon:${lon}`);
+  //   // second AJAX call for weather data , using lon and lat from the previous
+  //   return $.ajax({
+  //     url: queryURL,
+  //     method: "GET",
+  //   }).then(function (response) {
+  //     // console.log(response);
+  //     var results = response.list;
+  //     var timeStamps = [];
+  //     var currentTimestamp;
+  //     var fiveDaysData = [];
 
-    // second AJAX call for weather data , using lon and lat from the previous
-    return $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      // console.log(response);
-      var results = response.list;
-      var timeStamps = [];
-      var currentTimestamp;
-      var fiveDaysData = [];
-      results.map(function (item, i) {
-        if (i === 0) {
-          timeStamps.push(item.dt);
-          currentTimestamp = item.dt;
-          // console.log(currentTimestamp, item.dt_txt);
-          var firstDay = {
-            city: city,
-            date: moment.unix(item.dt).format("MM/DD/YYYY"),
-            icon: `http://openweathermap.org/img/w/${item.weather[0].icon}.png`,
-            temp: `${Math.round(item.main.temp - 273)} °C`,
-            humidity: `${item.main.humidity} %`,
-            wind: `${
-              Math.round(convertingWindToKph(item.wind.speed) * 100) / 100
-            } Km/h`,
-          };
-          fiveDaysData.push(firstDay);
-          console.log(firstDay);
-          console.log(fiveDaysData);
-        }
-        if (
-          item.dt === currentTimestamp + 86400 ||
-          item.dt === currentTimestamp + 2 * 86400 ||
-          item.dt === currentTimestamp + 3 * 86400 ||
-          item.dt === currentTimestamp + 4 * 86400
-        ) {
-          // console.log(item, " is one day later", item.dt_txt);
-          var futureDay = {
-            city: city,
-            date: moment.unix(item.dt).format("MM/DD/YYYY"),
-            icon: `http://openweathermap.org/img/w/${item.weather[0].icon}.png`,
-            temp: `${Math.round(item.main.temp - 273)} °C`,
-            humidity: `${item.main.humidity} %`,
-            wind: `${
-              Math.round(convertingWindToKph(item.wind.speed) * 100) / 100
-            } Km/h`,
-          };
-          fiveDaysData.push(futureDay);
-          timeStamps.push(item.dt);
-          console.log(fiveDaysData);
-        }
-        // console.log();
-      });
-      displayCurrentDay(fiveDaysData);
-      displayFiveDayForecast(fiveDaysData);
-      addCityToCities(city, fiveDaysData);
-      renderButtons(cities);
-      addToLocalStorage(cities);
-    });
-  });
+  //     // iterating through list of data and grabbing current data and 5 days in future by adding 24h in minutes in timestamp
+  //     results.map(function (item, i) {
+  //       if (i === 0) {
+  //         timeStamps.push(item.dt);
+  //         currentTimestamp = item.dt;
+  //         // console.log(currentTimestamp, item.dt_txt);
+  //         var firstDay = {
+  //           city: city,
+  //           date: moment.unix(item.dt).format("MM/DD/YYYY"),
+  //           icon: `http://openweathermap.org/img/w/${item.weather[0].icon}.png`,
+  //           temp: `${Math.round(item.main.temp - 273)} °C`,
+  //           humidity: `${item.main.humidity} %`,
+  //           wind: `${
+  //             Math.round(convertingWindToKph(item.wind.speed) * 100) / 100
+  //           } Km/h`,
+  //         };
+  //         fiveDaysData.push(firstDay);
+  //       }
+  //       if (
+  //         item.dt === currentTimestamp + 86400 ||
+  //         item.dt === currentTimestamp + 2 * 86400 ||
+  //         item.dt === currentTimestamp + 3 * 86400 ||
+  //         item.dt === currentTimestamp + 4 * 86400
+  //       ) {
+  //         var futureDay = {
+  //           city: city,
+  //           date: moment.unix(item.dt).format("MM/DD/YYYY"),
+  //           icon: `http://openweathermap.org/img/w/${item.weather[0].icon}.png`,
+  //           temp: `${Math.round(item.main.temp - 273)} °C`,
+  //           humidity: `${item.main.humidity} %`,
+  //           wind: `${
+  //             Math.round(convertingWindToKph(item.wind.speed) * 100) / 100
+  //           } Km/h`,
+  //         };
+  //         fiveDaysData.push(futureDay);
+  //         timeStamps.push(item.dt);
+  //       }
+  //     });
+  //     // below we re displaying current day , forecast for 5 days , adding city with data in cities state, adding it to localStorage if not already there and render buttons
+  //     displayCurrentDay(fiveDaysData);
+  //     displayFiveDayForecast(fiveDaysData);
+  //     addCityToCities(city, fiveDaysData);
+  //     renderButtons(cities);
+  //     addToLocalStorage(cities);
+  //   });
+  // });
 });
 
 //helper functions
@@ -155,17 +151,93 @@ function addCityToCities(city, fiveDaysData) {
 function addToLocalStorage(cities) {
   localStorage.setItem("cities", JSON.stringify(cities));
 }
+
 // function that adds button for city searched
 function renderButtons() {
   $("#history").empty();
 
   // Loops through the array of cities
-
   cities.map(function (ct) {
     var btnEl = $("<button>");
     btnEl.addClass("city");
     btnEl.attr("data-city", ct.cityName);
     btnEl.text(ct.cityName);
     $("#history").append(btnEl);
+  });
+}
+
+// Function that displays city info from button in history
+function displayCityInfo(city) {}
+
+// function that makes AJAX calls and handles data back
+function getCityForecastAndDisplay(city) {
+  var API_KEY = "678a5dc678523498214a6c93764d73f1";
+  var geolocationQueryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
+
+  $.ajax({
+    url: geolocationQueryURL,
+    method: "GET",
+  }).then(function (response) {
+    var lat = response[0].lat;
+    var lon = response[0].lon;
+
+    var queryURL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=50&appid=${API_KEY}`;
+
+    // second AJAX call for weather data , using lon and lat from the previous
+    return $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      // console.log(response);
+      var results = response.list;
+      var timeStamps = [];
+      var currentTimestamp;
+      var fiveDaysData = [];
+
+      // iterating through list of data and grabbing current data and 5 days in future by adding 24h in minutes in timestamp
+      results.map(function (item, i) {
+        if (i === 0) {
+          timeStamps.push(item.dt);
+          currentTimestamp = item.dt;
+          // console.log(currentTimestamp, item.dt_txt);
+          var firstDay = {
+            city: city,
+            date: moment.unix(item.dt).format("MM/DD/YYYY"),
+            icon: `http://openweathermap.org/img/w/${item.weather[0].icon}.png`,
+            temp: `${Math.round(item.main.temp - 273)} °C`,
+            humidity: `${item.main.humidity} %`,
+            wind: `${
+              Math.round(convertingWindToKph(item.wind.speed) * 100) / 100
+            } Km/h`,
+          };
+          fiveDaysData.push(firstDay);
+        }
+        if (
+          item.dt === currentTimestamp + 86400 ||
+          item.dt === currentTimestamp + 2 * 86400 ||
+          item.dt === currentTimestamp + 3 * 86400 ||
+          item.dt === currentTimestamp + 4 * 86400
+        ) {
+          var futureDay = {
+            city: city,
+            date: moment.unix(item.dt).format("MM/DD/YYYY"),
+            icon: `http://openweathermap.org/img/w/${item.weather[0].icon}.png`,
+            temp: `${Math.round(item.main.temp - 273)} °C`,
+            humidity: `${item.main.humidity} %`,
+            wind: `${
+              Math.round(convertingWindToKph(item.wind.speed) * 100) / 100
+            } Km/h`,
+          };
+          fiveDaysData.push(futureDay);
+          timeStamps.push(item.dt);
+        }
+      });
+      // below we re displaying current day , forecast for 5 days , adding city with data in cities state, adding it to localStorage if not already there and render buttons
+      displayCurrentDay(fiveDaysData);
+      displayFiveDayForecast(fiveDaysData);
+      addCityToCities(city, fiveDaysData);
+      renderButtons(cities);
+      addToLocalStorage(cities);
+    });
   });
 }
